15c48170537d959e6d483411be383ad6836a773c
JohnPosada <johnposada2000@gmail.com>
2022-07-26 01:55:01 -0500

    Se actualizaron las rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage, MarvelPage } from "../heroes/pages";
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
-
         <Route path="login" element={<LoginPage />} />
 
-        <Route path="/*" element={<Navigate to="marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
