406a34dedad0e2b935db9813c809dc7be70c5a22
JohnPosada <johnposada2000@gmail.com>
2022-07-28 19:44:45 -0500

    Se actualizo el loginPage y se añadio el authContext a HeroesApp

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
-
+  const { login } = useContext(AuthContext);
   const onLogin = () => {
-    navigate("/", {
+    login("John");
+    navigate(localStorage.getItem("path") || "/marvel", {
       replace: true,
     });
   };
