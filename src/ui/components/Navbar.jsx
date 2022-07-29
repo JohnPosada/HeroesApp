7cf92777901c2378dce26f4103fdb424b1486da6
JohnPosada <johnposada2000@gmail.com>
2022-07-28 19:34:19 -0500

    Se actualizo el navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context";
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
 
   const onLogout = () => {
+    logout();
     navigate("/login", {
       replace: true,
     });
@@ -34,12 +38,23 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link primary text-primary">User</span>
+          <span className="nav-item nav-link primary text-primary">
+            {user?.name}
+          </span>
           <button className="nav-item nav-link btn" onClick={onLogout}>
             Logout
           </button>
