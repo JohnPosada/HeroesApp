6b1c187feaf632d4a44fa40373c1b78f24626aa9
JohnPosada <johnposada2000@gmail.com>
2022-07-28 19:31:26 -0500

    Se declararon las rutas publicas y privadas

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../auth/context";
+
+export const PrivateRoute = ({ children }) => {
+  const { logged } = useContext(AuthContext);
+  const { pathname } = useLocation();
+  console.log(location);
+  localStorage.setItem("path", pathname);
+  return logged ? children : <Navigate to={"/login"} />;
+};
